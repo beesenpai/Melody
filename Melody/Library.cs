9fd27b6b6b0af392c3dd0da404e59a95acf011fc
beesenpai <elemaster96@aol.com>
2017-02-12 10:44:44 -0500

    Initial commit

---
+++ Melody/Library.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+namespace Melody.Core {
+    [Serializable]
+    public class Library {
+        private const string FNAME = "Melody.db";
+
+        private static string ResolveDirectory() {
+            if (Environment.OSVersion.Platform == PlatformID.Unix || Environment.OSVersion.Platform == PlatformID.MacOSX)
+                return Environment.GetEnvironmentVariable("HOME") + @"/.config/Melody/" + FNAME;
+            else return Environment.ExpandEnvironmentVariables("%APPDATA%") + @"\Melody\" + FNAME;
+        }
+
+        public static Library Load() {
+            if (File.Exists(ResolveDirectory())) {
+                Library lib;
+                IFormatter f = new BinaryFormatter();
+                FileStream io = new FileStream(ResolveDirectory(), FileMode.Open, FileAccess.Read, FileShare.Read);
+                try { lib = (Library)f.Deserialize(io); }
+                catch(SerializationException) { return new Library(); }
+                io.Close();
+                return lib;
+            }
+            else return new Library();
+        }
+
+        public void Save() {
+            IFormatter f = new BinaryFormatter();
+            if (!Directory.Exists(ResolveDirectory().Substring(0, ResolveDirectory().Length - 10)))
+                Directory.CreateDirectory(ResolveDirectory().Substring(0, ResolveDirectory().Length - 10));
+            FileStream io = new FileStream(ResolveDirectory(), FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.None);
+            f.Serialize(io, this);
+            io.Close();
+        }
+
+        ~Library() {
+            Save();
+        }
+    }
+}
---
+++ Melody/Song.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization;
+using TagLib;
+
+namespace Melody.Core {
+    public class Song {
+        public string FilePath { get; private set; }
+        public string Title { get; private set; }
+        public string[] Artists { get; private set; }
+        public string Album { get; private set; }
+        public IPicture AlbumArt { get; private set; }
+        public uint Year { get; private set; }
+        public string[] Genres { get; private set; }
+        public uint Track { get; private set; }
+        public uint TrackCount { get; private set; }
+        public uint Disc { get; private set; }
+        public uint DiscCount { get; private set; }
+        public string Lyrics { get; private set; }
+        public string Copyright { get; private set; }
+        public string[] Composers { get; private set; }
+        public string Conductor { get; private set; }
+        public uint BPM { get; private set; }
+        public bool Broken { get; private set; }
+
+        public Song(string filePath) {
+            FilePath = filePath;
+            var file = TagLib.File.Create(FilePath);
+            if (file == null) throw new FileNotFoundException();
+            Title = file.Tag.Title;
+            Artists = file.Tag.AlbumArtists;
+            AlbumArt = file.Tag.Pictures[0];
+            Genres = file.Tag.Genres;
+            Track = file.Tag.Track;
+            TrackCount = file.Tag.TrackCount;
+            Disc = file.Tag.Disc;
+            DiscCount = file.Tag.DiscCount;
+            Lyrics = file.Tag.Lyrics;
+            Copyright = file.Tag.Copyright;
+            Composers = file.Tag.Composers;
+            Conductor = file.Tag.Conductor;
+            BPM = file.Tag.BeatsPerMinute;
+        }
+    }
+}
